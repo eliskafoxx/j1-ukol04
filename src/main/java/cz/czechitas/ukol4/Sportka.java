433a9b5371d3bb4366be8002ad9a07d79ffb02b9
eliskafoxx <eliska.foxx@gmail.com>
2023-03-18 17:52:34 +0100

    Doplneny vsechny TO DO.

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.indexOfSubList;
+import static java.util.Collections.shuffle;
 /**
  * Třída pro losování Sportky.
  */
 public class Sportka {
   private final List<Integer> osudi = new ArrayList<>();
-
   public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
+    //naplnit osudí čísly 1 až 49.
+    for (int i = 0; i <= 49; i++) {
+      osudi.add(i);
+    }
   }
-
   /**
    * Zamíchá osudí.
    *
    * @see Collections#shuffle(List)
    */
   public void zamichat() {
-    //TODO zamíchat osudí
+    //zamíchat osudí
+    shuffle(osudi);
   }
-
   /**
    * Vrátí seznam s 6 vylosovanými čísly.
    *
@@ -31,17 +34,16 @@ public class Sportka {
    * @see List#subList(int, int)
    */
   public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
+    //Vrátit seznam prvních 6 čísel z osudí.
+    return osudi.subList(0,6);
   }
-
   /**
    * Vrátí sedmé vylosované číslo (dodatkové číslo)
    *
    * @return Dodatkové číslo.
    */
   public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return null;
+    //Vrátit sedmé číslo z osudí.
+    return osudi.get(6);
   }
 }
